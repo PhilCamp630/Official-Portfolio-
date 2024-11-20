074652c059a0de6831283b688eb07757f5471c70
PhilCamp630 <pcampbe8@asu.edu>
2024-11-20 01:26:55 -0700

    bottom nav bar for different dimensions fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,53 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 
 const Header = () => {
+  // Toggle Menu
+  const [Toggle, showMenu] = useState(false);
+
   return (
     <header className="header">
-        <nav className="nav container">
-            <a href="index.html" className='nav__logo'>Campbell</a>
-            <div className="nav_menu">
-              <ul className="nav_list grid">
-                  <li className="nav_item">
-                    <a href="#home" className="nav_link active-link">
-                      <i className="uil uil-home nav_icon"></i> Home
-                    </a>
-                  </li>
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Campbell</a>
 
-                  <li className="nav_item">
-                    <a href="#about" className="nav_link">
-                      <i className="uil uil-user nav_icon"></i> About
-                    </a>
-                  </li>
+        {/* Mobile Menu */}
+        <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
+          <ul className="nav_list grid">
+            <li className="nav_item">
+              <a href="#home" className="nav_link active-link">
+                <i className="uil uil-home nav_icon"></i> Home
+              </a>
+            </li>
+            <li className="nav_item">
+              <a href="#about" className="nav_link">
+                <i className="uil uil-user nav_icon"></i> About
+              </a>
+            </li>
+            <li className="nav_item">
+              <a href="#skills" className="nav_link">
+                <i className="uil uil-file-check-alt nav_icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav_item">
+              <a href="portfolio" className="nav_link">
+                <i className="uil uil-scenery nav_icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav_item">
+              <a href="#contact" className="nav_link">
+                <i className="uil uil-message nav_icon"></i> Contact
+              </a>
+            </li> 
+          </ul>
 
-                  <li className="nav_item">
-                    <a href="#skills" className="nav_link">
-                      <i className="uil uil-file-check-alt nav_icon"></i> Skills
-                    </a>
-                  </li>
+          {/* Close Button (only shown when menu is open) */}
+          <i className="uil uil-times nav_close" onClick={() => showMenu(false)}></i>
+        </div>
 
-                  <li className="nav_item">
-                    <a href="portfolio" className="nav_link">
-                      <i className="uil uil-scenery nav_icon"></i> Portfolio
-                    </a>
-                  </li>
-
-                  <li className="nav_item">
-                    <a href="#contact" className="nav_link">
-                      <i className="uil uil-message nav_icon"></i> Contact
-                    </a>
-                  </li> 
-              </ul>
-
-              <i className="uil uil-times nav_close"></i>
-            </div>
-
-            <nav className="nav_toggle">
-              <i className="uil uil-apps"></i>
-            </nav>
+        {/* Toggle Button (hamburger) */}
+        <nav className="nav_toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
         </nav>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
