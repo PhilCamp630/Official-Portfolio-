c64d48607855f4d2f858d0a10807349a82cf8ac5
PhilCamp630 <pcampbe8@asu.edu>
2024-11-24 19:02:55 -0700

    Home+Social.jsx edited and Data.jsx created

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home_social"> 
+
+        <a href="https://www.linkedin.com/in/phillipacampbell/" className="home_social-icon" target="_blank">
+            <i class="uil uil-linkedin"></i>
+        </a>
+            
+        <a href="https://github.com/PhilCamp630" className="home_social-icon" target="_blank">
+            <i class="uil uil-github"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
