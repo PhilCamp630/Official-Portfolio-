230b152237387cb80224012ce53452035b490e7a
PhilCamp630 <pcampbe8@asu.edu>
2024-11-29 17:36:29 -0700

    home h1, h3, and button added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./home.css"
 import Social from "./Social";
+import Data from "./Data";
 
 const Home = () => {
   return (
    <section className="home section" id="home">
     <div className="home_container container grid">
-        <div className="home_container grid">
+        <div className="home_content grid">
             <Social />
             <div className="home_img">
 
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import React from 'react'
-
 const Social = () => {
   return (
     <div className="home_social"> 
