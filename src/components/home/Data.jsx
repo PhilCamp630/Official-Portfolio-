de01798e75ec96a4d6bb7fadfa8eb30cf9c86336
PhilCamp630 <pcampbe8@asu.edu>
2024-11-29 19:45:10 -0700

    fixed home section position

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Data = () => {
   return (
-    <div className="homer_data">
+    <div className="home_data">
       <h1 className="home_title">Phillip Campbell
         <svg
                     width="36"
