c9739f6aed9b1503cf2663739abc788a2bb3e5ed
PhilCamp630 <pcampbe8@asu.edu>
2024-11-30 23:45:51 -0700

    changed vite config in hopes to fix deployment on render

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Custom output directory
+    outDir: 'dist', // Custom output directory
   },
 })
