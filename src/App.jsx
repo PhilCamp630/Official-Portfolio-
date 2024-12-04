b34b9430576355b73e0a63de1666d9ea81cbc026
PhilCamp630 <pcampbe8@asu.edu>
2024-12-04 03:40:03 -0700

    background progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import AnimatedCamouflage from './components/AnimatedCamouflage';
 
 function App() {
 
   return (
     <>
       <Header/>
-
+      <AnimatedCamouflage/>
       <main className="main">
         <Home/>
       </main>
