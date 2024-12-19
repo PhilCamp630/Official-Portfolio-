cb1f679b00a1b41d73f10d35245639327fe09cae
PhilCamp630 <pcampbe8@asu.edu>
2024-12-19 14:17:33 -0700

    info.jsx created

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -4,18 +4,18 @@ const Info = () => {
   return (
     <div className="about_info grid">
         <div className="about_box">
-            <h3 className="about_title"></h3>
-            <span className="about_subtitle"></span>
+            <h3 className="about_title">Experience</h3>
+            <span className="about_subtitle">TBA</span>
         </div>
 
         <div className="about_box">
-            <h3 className="about_title"></h3>
-            <span className="about_subtitle"></span>
+            <h3 className="about_title">TBA</h3>
+            <span className="about_subtitle">TBA</span>
         </div>
 
         <div className="about_box">
-            <h3 className="about_title"></h3>
-            <span className="about_subtitle"></span>
+            <h3 className="about_title">Projects</h3>
+            <span className="about_subtitle">TBA</span>
         </div>
     </div>
   )
